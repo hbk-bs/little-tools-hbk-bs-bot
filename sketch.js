6150cb068a3b9731c642d5b4c980a52c91153bd3
ff6347 <hash@ff6347.email>
2023-11-29 14:38:35 +0100

    psd 0.0.00000000000000002 alpha

--- sketch.js
+++ sketch.js
@@ -1,16 +1,34 @@
+const saveButton = document.querySelector('button#save');
+const colorInput = document.querySelector('input#color');
+console.log(colorInput);
+
+let paintColor = '#ff6347';
+colorInput.value = paintColor;
+
 function setup() {
-	createCanvas(500, 500);
+	const canvas = createCanvas(200, 200);
+	canvas.parent('sketch');
+	background(255);
+	saveButton.addEventListener('click', () => {
+		save('image.png');
+	});
+	colorInput.addEventListener('input', () => {
+		paintColor = colorInput.value;
+	});
 }
 
 function draw() {}
 
 function mouseDragged() {
+	fill(paintColor);
 	circle(mouseX, mouseY, 10);
 	line(pmouseX, pmouseY, mouseX, mouseY);
 	for (let i = 0; i < 100; i++) {
 		point(mouseX + random(-10, 10), mouseY + random(-10, 10));
 	}
 }
+
+function buttonHandler() {}
 
 function keyPressed() {
 	if (key === 's') {
